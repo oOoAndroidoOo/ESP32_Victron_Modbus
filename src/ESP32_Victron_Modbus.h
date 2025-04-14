283c8878ea2b2c67a5ad7338c959149bac471835
oOoAndroidoOo <michael.niemann@ewe.net>
2025-04-14 03:57:23 +0200

    modified:   src/ESP32_Victron_Modbus.h

--- src/ESP32_Victron_Modbus.h
+++ src/ESP32_Victron_Modbus.h
@@ -2,11 +2,13 @@
 
 #include <ModbusIP_ESP8266.h>
 #include <WiFi.h>
+
 // You can found a Registry of all available registers in the Victron Modbus documentation:
 // https://www.victronenergy.de/upload/documents/CCGX-Modbus-TCP-register-list-3.50.xlsx
 
 // Macro to define the register list using the X-Macro pattern.
 // Each entry consists of: Name, Modbus register address, device ID, conversion function, number of decimals.
+
 #define REGISTER_LIST                                    \
   X(REG_PV1, 811, 100, convertNone, 0)                   \
   X(REG_PV2, 808, 100, convertNone, 0)                   \
