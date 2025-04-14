53ada9703f009795eb659373b7b4df12e92cac50
oOoAndroidoOo <michael.niemann@ewe.net>
2025-04-14 11:16:57 +0200

    modified:   examples/main.cpp

--- examples/main.cpp
+++ examples/main.cpp
@@ -22,20 +22,16 @@ void setup() {
 
 void loop() {
   if (WiFi.status() == WL_CONNECTED) {
-   
-  //  modbus.readAllRegisters(); // bei Bedarf aktivieren
+  
    modbus.readAllRegisters();
    
-   Serial.println(modbus.getValue(REG_BAT_PWR));
-  // int socValue = modbus.readRegisterByName(REG_SOC);
+  Serial.println(modbus.getValue(REG_BAT_PWR));
+
   Serial.printf("🔋 BatterieLeistung      : %.2f W\n", modbus.readRegister(REG_BAT_PWR));
   Serial.printf("🔋 Batteriespannung      : %.2f V\n", modbus.readRegister(REG_Battery_Voltage));
   Serial.printf("🔋 Batteriestrom         : %.2f A\n", modbus.readRegister(REG_Battery_Current));
   Serial.printf("🔋 BatterieTemperatur    : %.2f C\n", modbus.readRegister(REG_Battery_Temp));
 
- // Serial.println(modbus.readRegister(REG_Battery_Voltage));
- // Serial.printf("🔋 Batter Strom     : %d\n",modbus.readRegister(REG_Battery_Current));
- // Serial.printf("🔋 Batter Temperatur: %d°C\n",modbus.readRegister(REG_Battery_Temp)/10);
   
     delay(10000);
   }
